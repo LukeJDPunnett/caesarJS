8017b261f0a55ec37e68654d08c0902870822a0a
Luke Punnett <31255258+LukeJDPunnett@users.noreply.github.com>
2017-09-30 21:19:23 +0100

    caesar cipher update
    now accurately converts letters and spaces.

--- caesar.js
+++ caesar.js
@@ -2,7 +2,12 @@ console.log('CaesarJS has loaded');
 //generate
 //functions which generate keys for relevant cyphers
 
+
 caesar = {
+	config:{
+		plainText: 'Hello my name is luke',
+		
+	},
 	info:"Caesar JS - Encrypt, Decrypt, Analyse and Learn using Front End Technologies. To learn more about using Caesar, use the command caesar.learn Author: Luke Punnett; Version: 0.1",
 	generate: {
 		info:"This information about the generate functions within Caesar JS",
@@ -13,17 +18,38 @@ caesar = {
 	encrypt:{
 		info:"Use this category of functions to encrypt data",
 		caesar:{
-			info:"allows you to encrypt using the Caesar Cipher. Takes 2 arguments, FIRST (INT): How many letters to shift the alphabet by, SECOND(STR): Message to be encrypted. use .example command to see an example",
-			go: function(shiftVal,set){
-				var testString = 'HELLO';
-				var ascValues = [];
-				var finalOutput = '';
-				for(var i=0;i<testString.length;i++){
-					ascValues.push((testString.charCodeAt(i))+shiftVal);
+			info: "allows you to encrypt using the Caesar Cipher. Takes 2 arguments, FIRST (INT): How many letters to shift the alphabet by, SECOND(STR): Message to be encrypted. use .example command to see an example",
+			go:function(shiftVal,set){
+				if(set == ''){
+					set = caesar.config.plainText;
+					console.log('caesar has used the value stored in the plainText value of the config object.');
 				}
+				var setLength = set.length;
+				//string to push character codes to be held in before converted into encrypted text.
+				var ascValues = [];
+				//string to hold result,
+				var finalOutput = '';
+				var pivotPoint = 122-shiftVal;
+				//loop, loop through each char in string
+				console.log('pivotPoint = '+pivotPoint);
+				for(var i=0;i<set.length;i++){
+					//TO BE DELETED FOR TEST DEVELOPMENT ONLY
+					
+					if(set.charCodeAt(i)<=pivotPoint && set.charCodeAt(i)!= 32){
+						ascValues.push((set.charCodeAt(i))+shiftVal);
+					}else if(set.charCodeAt(i)!=32){
+						ascValues.push((set.charCodeAt(i))-(26-shiftVal));
+					}else{
+						ascValues.push(set.charCodeAt(i));
+					}
+				}
+
 				for(var x=0;x<ascValues.length;x++){
 					finalOutput += String.fromCharCode(ascValues[x]);
 				}
+				//
+				console.log(ascValues);
+				//return the encoded text.
 				return finalOutput;
 			}
 		}
@@ -33,5 +59,8 @@ caesar = {
 	},
 	analyse:{
 		info:"Use this category of functions to analyse plain text / cypher text. For example frequency analysis."
+	},
+	utilities:{
+			// This object stores all the functions used internally within the core caesar js functions
 	}
 }
